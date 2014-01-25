9ee12e894dac0f943f26f38216c0d5b5d31cc1cc
nomis002 <nomis002b@gmail.com>
2014-01-25 23:03:17 +0100

    Update core.php

--- core.php
+++ core.php
@@ -28,30 +28,30 @@ function file_get_contents_curl($url)
 
 function getRedirectUrl($url)
 { 
-	 $redirect_url = null;
+	$redirect_url = null;
 
 	$url_parts = @parse_url($url);
 	if (!$url_parts) return false;
 	if (!isset($url_parts['host'])) return false; //can't process relative URLs
 	if (!isset($url_parts['path'])) $url_parts['path'] = '/';
 	$sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80), $errno, $errstr, 30);
-    if (!$sock) return false;
+	if (!$sock) return false;
 
-    $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n";
-    $request .= 'Host: ' . $url_parts['host'] . "\r\n";
-    $request .= "Connection: Close\r\n\r\n";
-    fwrite($sock, $request);
-    $response = '';
-    while(!feof($sock)) $response .= fread($sock, 8192);
-    fclose($sock);
+	$request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n";
+   	$request .= 'Host: ' . $url_parts['host'] . "\r\n";
+   	$request .= "Connection: Close\r\n\r\n";
+   	fwrite($sock, $request);
+   	$response = '';
+   	while(!feof($sock)) $response .= fread($sock, 8192);
+   	fclose($sock);
 
-    if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
-	    if ( substr($matches[1], 0, 1) == "/" )
-    	    return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
-        else
-        	return trim($matches[1]);
+	if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
+		if ( substr($matches[1], 0, 1) == "/" )
+			return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
+		else
+        		return trim($matches[1]);
         } else {
-            return false;
+		return false;
 	}
 }
 
@@ -60,22 +60,22 @@ function getAllRedirects($url)
 	$redirects = array();
     	while ($newurl = getRedirectUrl($url)){
         	if (in_array($newurl, $redirects)){
-            	break;
-            }
+        	    	break;
+        	}
             $redirects[] = $newurl;
             $url = $newurl;
-    }
+    	}
 	return $redirects;
 }
 
 function getFinalRedirect($url)
 {
 	$redirects = getAllRedirects($url);
-    if (count($redirects)>0){
-    	return array_pop($redirects);
-    } else {
-    	return $url;
-    }
+	if (count($redirects)>0){
+    		return array_pop($redirects);
+	} else {
+    		return $url;
+    	}
 }
 
 function get_inner_html($string, $start, $end)
